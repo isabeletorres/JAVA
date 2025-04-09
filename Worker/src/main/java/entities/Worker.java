8d50ed2acc68e8a98b1b00be4a5e1b671cba73dc
Isabele Torres <isabeledasilvatorres@gmail.com>
2025-04-09 18:26:33 -0300

    Finalizando o exercício utilizando enumerações e composições

--- Worker/src/main/java/entities/Worker.java
+++ Worker/src/main/java/entities/Worker.java
@@ -67,19 +67,20 @@ public class Worker {
         contracts.remove(contract);
     }
 
-    public Double income(int year, int month) {
+
+
+    public double income(int year, int month) {
         double sum = baseSalary;
         Calendar cal = Calendar.getInstance();
+
         for (HourContract c : contracts) {
             cal.setTime(c.getDate());
             int c_year = cal.get(Calendar.YEAR);
-            int c_month = cal.get(Calendar.MONTH);
-            if (c_year == year && c_month == month) {
+            int c_month = 1 + cal.get(Calendar.MONTH);
+            if (year == c_year && month == c_month) {
                 sum += c.totalValue();
             }
-
         }
         return sum;
     }
-
 }
