f07821b49da58e75a83aef18d8938e104a9abb90
Isabele Torres <isabeledasilvatorres@gmail.com>
2025-04-09 17:10:45 -0300

    Adicionando parte da resolução do problema worker

---
+++ Worker/src/main/java/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+
+import entities.Department;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter department's name: ");
+        String departmentName = sc.nextLine();
+        System.out.println("Enter worker data:");
+        System.out.print("Name: ");
+        String workerName = sc.nextLine();
+        System.out.print("Level: ");
+        String workerLevel = sc.nextLine();
+        System.out.print("Base Salary: ");
+        double baseSalary = sc.nextInt();
+
+        Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+
+    }
+}
