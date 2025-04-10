a8577f75bd77f7fc3c1dedc95d0ce0d15ebbbcb9
Isabele Torres <isabeledasilvatorres@gmail.com>
2025-04-10 09:38:58 -0300

    Adicionando a classe main e finalizando o projeto order

--- ExercicioComposicao/src/main/java/application/Main.java
+++ ExercicioComposicao/src/main/java/application/Main.java
@@ -1,7 +1,56 @@
 package application;
 
+import model.entities.*;
+import model.enums.OrderStatus;
+import java.text.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter client data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("E-mail: ");
+        String email = sc.next();
+        System.out.print("Birth date (DD/MM/YYYY): ");
+        Date birthDate = sdf.parse(sc.next());
+
+        Client client = new Client(name, email, birthDate);
+
+        System.out.println("Enter order data:");
+        System.out.print("Status: ");
+        OrderStatus orderStatus = OrderStatus.valueOf(sc.next());
+
+        Order order = new Order(new Date(), orderStatus, client);
+
+        System.out.print("How many items to this order? ");
+        int n = sc.nextInt();
+
+        for (int i =0; i<n; i++){
+            System.out.printf("Enter #%d item data:\n ", i+1);
+            System.out.print("Product name: ");
+            String nameProduct = sc.next();
+            System.out.print("Product Price: ");
+            double priceProduct = sc.nextDouble();
+
+            Product product = new Product(nameProduct, priceProduct);
+
+            System.out.print("Quantity: ");
+            int quantity = sc.nextInt();
+
+            OrderItem orderItem = new OrderItem(quantity, product, priceProduct);
+
+            order.addItem(orderItem);
+        }
+
+        System.out.println();
+        System.out.println("ORDER SUMARY: ");
+        System.out.println(order);
+
     }
 }
--- ExercicioComposicao/src/main/java/model/entities/Client.java
+++ ExercicioComposicao/src/main/java/model/entities/Client.java
@@ -1,8 +1,12 @@
 package model.entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Client {
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
     private String name;
     private String email;
     private Date birthDate;
@@ -37,5 +41,12 @@ public class Client {
 
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return  name +
+                "(" + sdf.format(birthDate)  + ") - " +
+                email;
     }
 }
