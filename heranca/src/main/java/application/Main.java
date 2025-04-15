2e138471a47571d945cabcd9b42c218792e2cbc5
Isabele Torres <isabeledasilvatorres@gmail.com>
2025-04-15 13:39:17 -0300

    Adicionando a classe account como classe abstrata

--- heranca/src/main/java/application/Main.java
+++ heranca/src/main/java/application/Main.java
@@ -2,25 +2,31 @@ package application;
 
 import entities.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
-        Account acc1 = new Account(1001, "Alex", 1000.0);
-        acc1.whithdraw(200.0);
+        List<Account> list = new ArrayList<>();
+        list.add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+        list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+        list.add(new SavingsAccount(1003, "Bob", 300.0, 0.01));
+        list.add(new BusinessAccount(1004, "Anna", 500.0, 500.0));
 
-        System.out.println(acc1.getBalance());
 
-        Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+        double sum = 0.0;
+        for (Account acc : list){
+            sum += acc.getBalance();
+        }
 
-        acc2.whithdraw(200.0);
+        System.out.println(sum);
+        for (Account acc : list){
+            acc.deposit(10.0);
+        }
 
-        System.out.println(acc2.getBalance());
-
-        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
-
-        acc3.whithdraw(200.0);
-
-        System.out.println(acc3.getBalance());
-
+        for (Account acc : list){
+            System.out.println(acc.getBalance());
+        }
     }
 }
